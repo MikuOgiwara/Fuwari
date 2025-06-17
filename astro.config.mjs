5c5023c1fa2aa1b7fb3dd2e576fc8532cdbb984c
MikuOgiwara <mikunakanosu@gmail.com>
2025-06-17 18:20:57 +0200

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://MikuOgiwara.github.io/",
-	base: "my-repo",
+	base: "Fuwari",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
